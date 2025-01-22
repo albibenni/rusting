988a41be9541afd72dae683c377c7e66917be1c9
Alberto Benatti <albi.benni8@gmail.com>
2025-01-22 10:31:43 +0100

    giving a sort of layout

--- src/basic/shadowing.rs
+++ src/basic/shadowing.rs
@@ -2,6 +2,10 @@ pub fn shadowing() {
     example(5);
 }
 
+fn main() {
+    example(5);
+}
+
 fn example(param: i32) -> () {
     println!("Param before new scope: {}", param);
 
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #[path = "./basic/shadowing.rs"]
 mod shadowing;
 fn main() {
-    print!("Hello I'm rusting")
-    //shadowing::shadowing();
+    print!("Hello I'm rusting");
+    shadowing::shadowing();
 }
