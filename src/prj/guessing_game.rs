399b3a23eaa3607c25bc399a70f6f268eeee0f20
Alberto Benatti <albi.benni8@gmail.com>
2025-01-25 11:08:25 +0100

    guess and rng generator with checker

--- src/prj/guessing_game.rs
+++ src/prj/guessing_game.rs
@@ -1,4 +1,5 @@
-//use std::io; // alternative way to import
+use rand::Rng;
+
 pub fn guess() -> () {
     println!("Guess the number!");
 
@@ -8,5 +9,14 @@ pub fn guess() -> () {
         .read_line(&mut guess)
         .expect("Failed to read line"); // without expect it won't compile - possible error
 
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    println!("The secret number is: {secret_number}");
     println!("You guessed: {}", guess);
+    match guess.cmp(&secret_number) {
+        std::cmp::Ordering::Less => println!("Too small!"),
+        std::cmp::Ordering::Greater => println!("Too big!"),
+        std::cmp::Ordering::Equal => println!("You win!"),
+    }
 }
