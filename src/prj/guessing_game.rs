04830d4d7387161c7190b9bcfd060c453ef2e155
Alberto Benatti <albi.benni8@gmail.com>
2025-01-25 11:17:50 +0100

    match is cool

--- src/prj/guessing_game.rs
+++ src/prj/guessing_game.rs
@@ -1,22 +1,29 @@
 use rand::Rng;
 
 pub fn guess() -> () {
-    println!("Guess the number!");
-
-    let mut guess = String::new();
-
-    std::io::stdin() // alternative to the import and call io:stdin() fucntion
-        .read_line(&mut guess)
-        .expect("Failed to read line"); // without expect it won't compile - possible error
-
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+    loop {
+        println!("Guess the number!");
 
-    println!("The secret number is: {secret_number}");
-    println!("You guessed: {}", guess);
-    match guess.cmp(&secret_number) {
-        std::cmp::Ordering::Less => println!("Too small!"),
-        std::cmp::Ordering::Greater => println!("Too big!"),
-        std::cmp::Ordering::Equal => println!("You win!"),
+        let mut guess = String::new();
+
+        std::io::stdin() // alternative to the import and call io:stdin() fucntion
+            .read_line(&mut guess)
+            .expect("Failed to read line"); // without expect it won't compile - possible error
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+        match guess.cmp(&secret_number) {
+            std::cmp::Ordering::Less => println!("Too small!"),
+            std::cmp::Ordering::Greater => println!("Too big!"),
+            std::cmp::Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
+    println!("The secret number is: {secret_number}");
 }
