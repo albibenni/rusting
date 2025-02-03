82ccf7207463dd13fe2f8f3947a098ac40b0b385
Alberto Benatti <albi.benni8@gmail.com>
2025-02-03 15:22:12 +0100

    copying owneship

--- src/basic/ownership.rs
+++ src/basic/ownership.rs
@@ -8,13 +8,21 @@ fn plus_one(n: i32) -> i32 {
     n + 1
 }
 
-pub fn heap_add_suffix() {
-    let first = String::from("albi");
-    let full = add_second(first);
-    println!("{full}");
-}
-
 fn add_second(mut name: String) -> String {
     name.push_str("benni");
     name
+}
+
+pub fn heap_add_suffix() {
+    let first = String::from("albi");
+    let full = add_second(first);
+    // first is moved --
+    println!("{full}");
+}
+
+pub fn cloning_heap_add_suffix() {
+    let first = String::from("albi");
+    let first_clone = first.clone();
+    let full = add_second(first_clone);
+    println!("now it's possible to use the first: {first} and the concatenated: {full}");
 }
--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,13 @@ mod array_and_slices;
 mod ownership;
 
 fn main() {
-    println!("---- Hello I'm rusting ---");
+    println!("---- Hello I'm rusting ----");
     //shadowing::shadowing();
     //guessing_game::guess();
     // let res = functions::return_value();
     // println!("return value: {res}");
     ownership::owning_basic();
     ownership::heap_add_suffix();
-    println!("---- End rusting ---");
+    ownership::cloning_heap_add_suffix();
+    println!("---- End rusting ----");
 }
