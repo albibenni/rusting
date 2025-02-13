5c325905ed6195a568cd5ef06e4ccfa3c97ddfc4
Alberto Benatti <albi.benni8@gmail.com>
2025-02-13 11:23:17 +0100

    rust kinda cool with impls

--- src/basic/enum_try.rs
+++ src/basic/enum_try.rs
@@ -14,16 +14,33 @@ impl Color {
         static COLORS: [Color; 4] = [Blue, Green, Yellow, Red];
         COLORS.iter()
     }
+    fn is_green(&self) -> bool {
+        if let Color::Green = self {
+            return true;
+        }
+        return false;
+    }
+    fn is_green_parts(&self) -> bool {
+        match self {
+            Blue => return true,
+            Green => return false,
+            Yellow => return true,
+            Red => return false,
+        }
+    }
 }
 pub fn use_enum() {
     for color in Color::iterator() {
         match color {
-                Color::Blue => println!("blue"),
-                Color::Green => println!("Green"),
-                Color::Yellow => println!("Yellow"),
-                Color::Red => println!("red"),
-            };
+            Color::Blue => println!("blue"),
+            Color::Green => println!("Green"),
+            Color::Yellow => println!("Yellow"),
+            Color::Red => println!("red"),
+        };
     }
+
+    let foo_green = Color::Green.is_green();
+    let foo_blue= Color::Green.is_green_parts();
+    println!("using impl is green, {}", foo_green );
+    println!("using impl is green part, {}", foo_blue );
 }
-
-
