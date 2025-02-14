e8e63ca96b5ed256179475b223aa9e22606c0239
Alberto Benatti <albi.benni8@gmail.com>
2025-02-14 18:44:05 +0100

    ? super cool

--- src/basic/option.rs
+++ src/basic/option.rs
@@ -22,6 +22,15 @@ pub fn using_option() {
     println!("Option evaluation of some none : {}", res_und);
     println!("Option evaluation of some num -- better: {}", res_b);
     println!("Option evaluation of some none -- better : {}", res_b_und);
+
+    let ret_und_too = using_map(non.clone());
+    let ret_und_but_no = using_map(Some(3));
+    let ret_und_tooo = using_map_and_convert(non.clone());
+    let ret_und_but_noo = using_map_and_convert(Some(3));
+    println!("Ret undefined - num: {:?}", ret_und_but_no);
+    println!("Ret undefined - und: {:?}", ret_und_too);
+    println!("Ret undefined with ? - num: {:?}", ret_und_but_noo);
+    println!("Ret undefined with ? - und: {:?}", ret_und_tooo);
 }
 
 fn ret_a_num_with_undefined(val: Option<usize>) -> usize {
@@ -33,4 +42,17 @@ fn ret_a_num_with_undefined(val: Option<usize>) -> usize {
 
 fn better_ret(val: Option<usize>) -> usize {
     return val.unwrap_or(0) * 5;
+}
+
+fn using_map(val: Option<usize>) -> Option<usize> {
+    return val.map(|x| x * 5);
+}
+
+fn using_map_and_convert(val: Option<usize>) -> Option<usize> {
+    // ? does:
+    // let num = match val {
+    //     Some(x) => x,
+    //     None => return None,
+    // };
+    return Some(val? * 5);
 }
