0ab3887e7d34c8801575578c5a9f1908adcc69ec
Alberto Benatti <albi.benni8@gmail.com>
2025-02-20 10:59:54 +0100

    borrow in struct

--- src/basic/borrow/struct.rs
+++ src/basic/borrow/struct.rs
@@ -6,10 +6,22 @@ struct Item {
 fn add_one(item: &mut Item) {
     item.count += 1;
 }
+
+fn print_all(items: &Vec<Item>) {
+    for item in items {
+        println!("{:?}", item);
+    }
+}
 pub fn doit() {
     let mut item = Item { count: 1 };
     println!("{:?}", item);
 
     add_one(&mut item);
     println!("{:?}", item);
+
+    let mut itemm = vec![Item { count: 1 }];
+
+    let first = itemm.first_mut();
+    println!("{:?}", first);
+    print_all(&itemm); // works because after first -- borrowed
 }
