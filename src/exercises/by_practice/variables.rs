0d76e767130a35323e83280e11f2f9342c76c2b4
Alberto Benatti <albi.benni8@gmail.com>
2025-02-28 09:59:37 +0100

    var

--- src/exercises/by_practice/variables.rs
+++ src/exercises/by_practice/variables.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+
 pub fn variab() {
     let x: i32 = 5; // Uninitialized but used, ERROR !
     let _y: i32 = 1; // Uninitialized but also unused, only a Warning !
@@ -7,8 +9,20 @@ pub fn variab() {
 
     let x = define_x();
     println!("{}, world", x);
+
+    dest();
 }
 
 fn define_x() -> &'static str {
     return "hello";
+}
+
+fn dest() {
+    let (x, y);
+    (x, ..) = (3, 4);
+    [.., y] = [1, 2];
+    println!("{},{}", x, y);
+    assert_eq!([x, y], [3, 2]);
+
+    println!("Success!");
 }
